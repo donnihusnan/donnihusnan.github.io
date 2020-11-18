3e9d1e3c9ac851312a1f94cd02f697909dd6e8d4
Eden <donnihusnan@gmail.com>
2020-11-18 21:07:49 +0700

    tinggal balikin

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,19 @@ function puter() {
 
   let gravityDownLeft = document.querySelectorAll(".boxes__left > .box");
   for (let i = 0; i < gravityDownLeft.length; i++) {
-    gravityDownLeft[i].style.animation = "gravitydown 5s linear infinite";
+    gravityDownLeft[0].style.animation = "gravitydown 10s linear infinite";
+    gravityDownLeft[1].style.animation = "gravitydown 8s linear infinite";
+    gravityDownLeft[2].style.animation = "gravitydown 7s linear infinite";
+    gravityDownLeft[3].style.animation = "gravitydown 9s linear infinite";
+    gravityDownLeft[4].style.animation = "gravitydown 9s linear infinite";
   }
 
   let gravityDownRight = document.querySelectorAll(".boxes__right > .box");
   for (let i = 0; i < gravityDownRight.length; i++) {
-    gravityDownRight[i].style.animation = "gravityup 5s linear infinite";
+    gravityDownRight[0].style.animation = "gravityup 8s linear infinite";
+    gravityDownRight[1].style.animation = "gravityup 12s linear infinite";
+    gravityDownRight[2].style.animation = "gravityup 7s linear infinite";
+    gravityDownRight[3].style.animation = "gravityup 9s linear infinite";
+    gravityDownRight[4].style.animation = "gravityup 11s linear infinite";
   }
 }
