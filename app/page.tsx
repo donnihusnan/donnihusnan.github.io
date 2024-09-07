01cbabfb3496901cb2218a4cb463631a648f6d22
Donni Husnan <donnihusnan@gmail.com>
2024-09-07 21:25:05 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,88 @@
-import Image from "next/image";
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { Github, Linkedin } from 'lucide-react';
+import IconCloud from '@/components/magicui/icon-cloud';
+import Meteors from '@/components/magicui/meteors';
+
+const slugs = [
+  'typescript',
+  'javascript',
+  'react',
+  'html5',
+  'css3',
+  'nodedotjs',
+  'nextdotjs',
+  'amazonaws',
+  'firebase',
+  'vercel',
+  'git',
+  'github',
+  'visualstudiocode',
+  'tailwindcss',
+  'bootstrap',
+  'basecamp',
+  'vuedotjs',
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="relative flex flex-col min-h-screen w-full overflow-hidden bg-gradient-to-tr from-gray-50 via-gray-150 to-gray-200 dark:bg-gray-900">
+      <Meteors number={30} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex-grow flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 p-6 md:p-16 lg:p-24 flex flex-col justify-center">
+          <div className="max-w-md mx-auto text-center mt-16 md:text-left">
+            <header className="mb-6 md:mb-8">
+              <p className="text-sm font-semibold md:text-base text-gray-600 dark:text-gray-300">
+                Hello There... My Name is{' '}
+              </p>
+              <h1 className="mb-2 text-3xl md:text-5xl font-bold text-gray-800 dark:text-white">
+                Donni Husnan
+              </h1>
+              <h2 className="text-lg md:text-xl font-semibold text-gray-600 dark:text-gray-300">
+                a Self Taught Web Dev
+              </h2>
+            </header>
+
+            <section className="mb-6 md:mb-8">
+              <p className="text-sm md:text-base text-gray-600 dark:text-gray-300">
+                I'm having keen interest in creating efficient solutions. My
+                journey in web development has equipped me with a diverse skill
+                set and a problem-solving mindset.
+              </p>
+            </section>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 p-6 flex flex-col justify-center">
+          <div className="flex-grow mx-auto">
+            <IconCloud iconSlugs={slugs} />
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="py-4 text-center">
+        <div className="mb-2">
+          <a
+            href="https://www.linkedin.com/in/your-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block mr-4 text-gray-600 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+          >
+            <Linkedin size={24} />
+          </a>
+          <a
+            href="https://github.com/your-username"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white"
+          >
+            <Github size={24} />
+          </a>
+        </div>
+        <p className="text-xs md:text-sm text-gray-500 dark:text-gray-400">
+          &copy; {new Date().getFullYear()} Donni Husnan. All rights reserved.
+        </p>
       </footer>
     </div>
   );
